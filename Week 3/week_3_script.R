3ec482d9c44f71875fa2742c06145aaf1f93e3d5
romele-stefano <romele.stefano@gmail.com>
2019-05-29 08:45:59 +0200

    Added interpretation to the exp.smoothing function

--- Week 3/week_3_script.R
+++ Week 3/week_3_script.R
@@ -1,6 +1,7 @@
 setwd("C:/Users/ROMEST/Downloads/homework/Week 3")
 library("smooth")
 
+set.seed(888)
 data <- read.table("temps.txt", header = T)
 
 # create vector of data by unlisting the data frame
@@ -11,6 +12,9 @@ plot(data.vc, type = "l")
 
 # model
 HW.fit <- es(data.vc, model = "AAM")
+# the model gives "beta" value of 0.000. "Beta" is the trand smoothing function
+# a value near 0 means there is no significant trend, suggesting there is not a 
+# significant increase or decrease over the years.
 
 HW.fit[4]
 
@@ -25,39 +29,3 @@ y <- y[1:length(data.vc)]
 # add line for predicted values on previous graph
 lines(x, y, col = "blue")
 
-
-# apply CUSUM to predicted values
-
-# find avg temperature
-# avgtemp <- mean(y)
-# 
-# 
-# # initialize variable
-# st <- 0
-# C <- 3
-# # looping for cusum
-# # St= max{0,St???1+ (xt????????C)}
-# for (i in 1:length(y)){
-#   # first loop
-#   if (i == 1) {
-#     # average over the years (data$avg)
-#     if (0 + (y[i] - avgtemp - C) > 0){
-#       st[i] <- 0 + (y[i] - avgtemp - C)
-#     } else {
-#       st[i] <- 0
-#     }
-#   } else {
-#     if (st[i-1] + (y[i] - avgtemp - C) > 0){
-#       st[i] <- st[i-1] + (y[i] - avgtemp - C)
-#     } else {
-#       st[i] <- 0
-#     }
-#   }
-# }
-# 
-# plot(st, type = "l")
-# # find data point in data set associated with max value of cusum
-# y[which.max(st)]
-# 
-# 
-# split.data <- split(y, ceiling(seq_along(y)/122))
