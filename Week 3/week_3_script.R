fd8a0d9ba192f72b381f71f18156ae8c8eb64a7c
romele-stefano <romele.stefano@gmail.com>
2019-05-30 08:23:18 +0200

    Added regression

--- Week 3/week_3_script.R
+++ Week 3/week_3_script.R
@@ -29,3 +29,28 @@ y <- y[1:length(data.vc)]
 # add line for predicted values on previous graph
 lines(x, y, col = "blue")
 
+# plot trend (EVALUATE)
+trend <- HW.fit[10]
+trend <- unlist(trend)
+abline(trend[1], trend[2], col = "red")
+
+
+
+
+
+## REGRESSION
+
+set.seed(888)
+data <- read.table("uscrime.txt", header = T)
+
+# train the model on all predictors
+glm.fit <- glm(Crime ~ ., family = "gaussian", data)
+
+# data point to use for prediction
+p <- cbind(14,0,10,12,15.5,0.640,94,150,1.1,0.120,3.6,3200,20.1,0.04,39)
+p <- as.data.frame(p)
+colnames(p) <- c("M","So","Ed","Po1","Po2","LF","M.F","Pop","NW","U1","U2","Wealth","Ineq","Prob","Time")
+
+
+# prediction
+prediction <- predict(glm.fit, newdata = p)
