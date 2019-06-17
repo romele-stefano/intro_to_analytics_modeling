e9b3cbc24337c22034b9160d57bc603d30f9666f
romele-stefano <romele.stefano@gmail.com>
2019-06-17 09:07:44 +0200

    Minor fix

--- Week 5/week 5.R
+++ Week 5/week 5.R
@@ -1,4 +1,5 @@
 setwd("C:/Users/ROMEST/Downloads/homework/Week 5")
+setwd("C:/USers/romel/Downloads/Week 5")
 
 set.seed(888)
 
@@ -21,9 +22,10 @@ lm.fit.full <- lm(Crime ~ .,data = data)
 lm.fit.initial <- lm(Crime ~ 1, data = data)
 
 # create stepwise
-step.fit <- step(lm.fit.initial, scope = list(lower = lm.fit.initial, upper = lm.fit.full), direction = "both")
+step.fit <- step(lm.fit.initial, scope = list(lower = lm.fit.initial, upper = lm.fit.full), direction = "both", trace = 1)
 summary(step.fit)
 step.fit$anova
+
 
 # create regression model 
 lm.fit.stepwise <- lm(Crime ~ Po1+Ineq+Ed+M+Prob+U2, data = data)
@@ -74,27 +76,20 @@ coef(lasso.fit, s = lasso.fit$lambda.1se)
 ###
 
 # regression model for lambda.min
-lasso.fit.lambdamin <- lm(Crime ~ M+So+Ed+Po1+M.F+NW+U1+U2+Wealth+Ineq+Prob, data = data)
+lasso.fit.lambdamin <- lm(Crime ~ M+So+Ed+Po1+LF+M.F+NW+U1+U2+Ineq+Prob, data = data)
 summary(lasso.fit.lambdamin)
-# Remove variables with p-valuer > 0.05
-lasso.fit.lambdamin.minimized <- lm(Crime ~ M+So+Po1+U2+Ineq+Prob, data = data)
-# anova between the two models
-# https://bookdown.org/ndphillips/YaRrr/comparing-regression-models-with-anova.html
-anova(lasso.fit.lambdamin, lasso.fit.lambdamin.minimized)
+
 
 # regression model for lambda.1se
 lasso.fit.1se <- lm(Crime ~ M+So+Ed+Po1+M.F+NW+U2+Ineq+Prob, data = data)
 summary(lasso.fit.1se)
-# Remove variables with p-valuer > 0.05
-lasso.fit.1se.minimized <- lm(Crime ~ M+Ed+Po1+U2+Ineq+Prob, data = data)
-# anova between the two models
-# https://bookdown.org/ndphillips/YaRrr/comparing-regression-models-with-anova.html
-anova(lasso.fit.1se, lasso.fit.1se.minimized)
 
 
 
 # model: ridge regression
-elastic <- glmnet(predictors, response, family = "mgaussian", alpha = 0.5)
+elastic <- glmnet(predictors, response, alpha = 0.5)
+# plotmo
+plot_glmnet(elastic)
 
 # find best lambda value
 elastic.fit <- cv.glmnet(predictors, response, alpha = 0.5)
@@ -113,27 +108,22 @@ coef(elastic.fit, s = elastic.fit$lambda.1se)
 # regression model for lambda.min
 elastic.fit.lambdamin <- lm(Crime ~ M+So+Ed+Po1+Po2+M.F+Pop+NW+U1+U2+Wealth+Ineq+Prob, data = data)
 summary(elastic.fit.lambdamin)
-# Remove variables with p-valuer > 0.05
-elastic.fit.lambdamin.minimized <- lm(Crime ~ M+Po1+Po2+U2+Ineq+Prob, data = data)
-# anova between the two models
-# https://bookdown.org/ndphillips/YaRrr/comparing-regression-models-with-anova.html
-anova(elastic.fit.lambdamin, elastic.fit.lambdamin.minimized)
+
 
 # regression model for lambda.1se
 elastic.fit.1se <- lm(Crime ~ M+Po1+Po2+M.F+NW+Ineq+Prob, data = data)
 summary(elastic.fit.1se)
-# Remove variables with p-valuer > 0.05
-elastic.fit.1se.minimized <- lm(Crime ~ M.F+Ineq+Prob, data = data)
-# anova between the two models
-# https://bookdown.org/ndphillips/YaRrr/comparing-regression-models-with-anova.html
-anova(elastic.fit.1se, elastic.fit.1se.minimized)
+
 
 
 ########################################################################################
 ################### TRY TO NORMALIZED DATE FOR STEPWISE BEFORE ANOVA ###################
 ########################################################################################
 # compare stepwise, lasso and elastic
-anova(lm.fit.stepwise, lasso.fit.lambdamin, elastic.fit.lambdamin)
+# step = 6 predictors, elastic1se = 7 predictors, lasso1se = 9 predictors
+# lassolamb = 11 predictors, elasticlam = 13 predictors
+anova(lm.fit.stepwise, elastic.fit.1se, lasso.fit.1se, lasso.fit.lambdamin, elastic.fit.lambdamin)
+
 
 
 
